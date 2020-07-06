808fa20f3b283b5b4c3398906f9ae75aec955d1c
repl.it user <replituser@example.com>
2020-07-06 13:24:12 +0000

    Proyecto Examen

---
+++ ExamenMVN/src/main/java/pe/edu/upeu/dmp/App.java
@@ -0,0 +1,15 @@
+package pe.edu.upeu.dmp;
+
+/**
+ * Hello world!
+ *
+ */
+import pe.edu.upeu.dmp.examen.*;
+public class App 
+{
+    public static void main( String[] args ){
+        System.out.println( "Hello World!" );
+      ExamenDMP obj=new ExamenDMP();
+      obj.saludo();
+    }
+}
---
+++ ExamenMVN/src/main/java/pe/edu/upeu/dmp/examen/ExamenDMP.java
@@ -0,0 +1,9 @@
+package pe.edu.upeu.dmp.examen;
+
+public class ExamenDMP{
+
+public void saludo(){
+  System.out.println("Holas Repl Como estan!!!");
+}
+
+}
